248dcc6d9a25ec95245f91129e00e955c5b51f5a
KyiMoeTun <thisismyveryfirstemailaddress@gmail.com>
2023-02-04 23:48:26 -0600

    Initial commit

---
+++ class_test.py
@@ -0,0 +1,62 @@
+class Account(object):
+    'a bank account class'
+
+    def set(self, value):
+        'set the balance to value'
+        self.balance = value
+
+    def get(self):
+        'return the current balance on the account'
+        return self.balance
+
+    def deposit(self, value):    
+        'updates the bank account by depositing value amount'
+        self.balance += value      
+
+    def withdraw(self, value):  
+        'updates the bank account by withdrawing value amount'
+        self.balance -= value
+
+    def __init__(self, value = 0.0):
+        'the constructor'
+        self.balance = value
+
+    def __repr__(self):
+        return f"Account({self.balance})"
+
+    def __str__(self):
+        return f"${self.balance:.2f}"
+
+class Savings(Account):
+    'a savings account'
+
+    def __init__(self, amount = 100.0, rate = 0.001):
+        'the constructor'
+        Account.__init__(self, amount)
+        self.rate = rate
+    
+    def setRate(self, value):
+        'set the yearly interest rate'
+        self.rate = value
+
+    def addInterest(self):
+        'add one month of interest to the balance'
+        self.balance += self.balance * (self.rate / 12)
+        
+    def get(self):
+        'prints the value of the balance'
+        print(str(self))
+
+    def __repr__(self):
+        return f"Savings({self.balance}, {self.rate})"
+
+    def __str__(self):
+        return f'balance = ${self.balance:.2f}\nrate = {self.rate}%'
+
+def cheer(n):
+    'return a string with a silly cheer based on n'
+    if n <= 1:
+        return "Hurrah!"
+    else: 
+        return "Hip " + cheer(n-1)
+
